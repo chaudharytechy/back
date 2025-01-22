27970f31227285f8eef4ee12b5245c45554a55f2
chaudharytechy <amitchaud10@gmail.com>
2025-01-22 21:59:36 +0530

    jason sdhgf

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const express = require('express')
 const app = express()
 const port = 3500
-
 app.get('/api', (req, res) => {
-  res.send('Hello World!')
-})
+  res.json({ message: 'Hello World!' });
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
